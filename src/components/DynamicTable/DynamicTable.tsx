1fa761f3e4acddb43a0a4583eed53b45f8ad3938
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2023-08-02 10:42:37 -0600

    prop import fix

--- src/components/DynamicTable/DynamicTable.tsx
+++ src/components/DynamicTable/DynamicTable.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import DynamicTableM from '@atlaskit/dynamic-table';
-import { StatefulProps } from "@atlaskit/dynamic-table/dist/types/types";
+import { StatefulProps } from "@atlaskit/dynamic-table/types";
 
 /**
  * @uxpindocurl https://atlassian.design/components/DynamicTable/examples
