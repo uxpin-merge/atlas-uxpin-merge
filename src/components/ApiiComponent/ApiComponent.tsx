e99a29e600094049c2fbbae3143344fe2b67e6ca
Robert Hilscher <r.hilscher@gmail.com>
2023-08-07 12:18:48 +0200

    use storybook proxy for jira request

--- src/components/ApiiComponent/ApiComponent.tsx
+++ src/components/ApiiComponent/ApiComponent.tsx
@@ -5,19 +5,20 @@ const ApiComponent = () => {
   const [issues, setIssues] = useState([{key: 0, fields: {summary: ''}}]);
 
   useEffect(() => {
+    const uxpinProxy = 'https://app.uxpin.com/storybook';
     // Your Jira instance URL
-    const jiraBaseUrl = 'https://jack-uxpin.atlassian.net';
+    const jiraBaseUrl = 'https://rob-test-uxpin.atlassian.net';
     // Your Jira project key
-    const projectKey = 'TODO';
+    const projectKey = 'test-project';
     // Replace 'YOUR_USERNAME' and 'YOUR_API_TOKEN' with your Jira credentials
-    const username = 'jack@uxpin.com';
-    const apiToken = 'ATCTT3xFfGN0bbJjSkAchXRHSFT3JYSvq34gWVz7WMtLES-qhqow8RbMmyLw4hGLyU_D0wQA4DmKkM8q4VnNXe5CBa_pgaVJPMxzkKz7-vKRp4mKFXPWhQTaFgWhv1J55SxZpWf8wxFzAPco9djDqlReeh3maPoKGUhBoTlXW5HmV4-n3j0zltU=1E2FFA88';
+    const username = 'robert.hilscher@uxpin.com';
+    const apiToken = 'ATATT3xFfGF0C0tZj0jXBh-tTlD5BugCBPrRbkuiN2zFqlzWVwWbZb7zyEXVBkjInCt8oS_6mO1g3JLYRVUkftsKhpCkrMxUG0NaR3CnzLJS9aA176EKyf_Fk7y0AEZazSgeFf20G3OJYmerDlqdkRbmsA10AoIWEO0JVEqVDSRkY2nE4yLJxI0=5B658202';
     const auth = btoa(`${username}:${apiToken}`);
 
     const fetchIssues = async () => {
       try {
         const response = await axios.get(
-          `${jiraBaseUrl}/rest/api/2/search?jql=project=${projectKey}`,
+          `${uxpinProxy}/${jiraBaseUrl}/rest/api/2/search?jql=project=${projectKey}`,
           {
             headers: {
               Authorization: `Basic ${auth}`,
