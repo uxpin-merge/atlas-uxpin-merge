fda783e32f5a58b998216630525bd56335aae6f5
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2023-08-08 13:46:28 -0600

    cleanup

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,21 +1,9 @@
 import React from "react";
-// import Form, {
-//   CheckboxField,
-//   ErrorMessage,
-//   Field,
-//   FormFooter,
-//   FormHeader,
-//   FormSection,
-//   HelperMessage,
-//   RequiredAsterisk,
-//   ValidMessage,
-// } from '@atlaskit/form';
+import TextFieldM, {TextFieldProps} from '@atlaskit/textfield';
 
 /**
  * @uxpindocurl https://atlassian.design/components/textfield/examples
  */
-import TextFieldM, {TextFieldProps} from '@atlaskit/textfield';
-
 function TextField(props: TextFieldProps) {
   return (
     <TextFieldM {...props}/>
